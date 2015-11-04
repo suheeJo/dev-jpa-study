1996ece35aa3aa0030afda7e1d8d5ed08bf7c6a9
Minyoung Kim <cyzest@nate.com>
2015-11-04 16:01:53 +0900

    JPA 기본 구조 커밋

---
+++ src/main/java/com/dev/jpa/Member.java
@@ -0,0 +1,23 @@
+package com.dev.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+//@Table(name="MST_MEMBER")
+public class Member {
+	
+	 @Id
+	 private String id;
+	 
+	 @Column(name="name")
+	 private String userName;
+	 
+	 private Integer age;
+	 
+}
